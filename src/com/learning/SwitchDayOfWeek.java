f98be350765a66fb49546076e6b12349c780c42e
Andriy_Chmykhal <chmykhal@ukr.net>
2017-03-05 15:59:47 +0200

    syntax issues

---
+++ src/com/learning/SwitchDayOfWeek.java
@@ -0,0 +1,37 @@
+package com.learning;
+
+public class SwitchDayOfWeek {
+    public static void main(String[] args) {
+
+        int day = 13;
+        String dayString;
+        switch (day) {
+            case 1:
+                dayString = "Monday";
+                break;
+            case 2:
+                dayString = "Tuesday";
+                break;
+            case 3:
+                dayString = "Wednesday";
+                break;
+            case 4:
+                dayString = "Thursday";
+                break;
+            case 5:
+                dayString = "Friday";
+                break;
+            case 6:
+                dayString = "Saturday";
+                break;
+            case 7:
+                dayString = "Sunday";
+                break;
+            default:
+                dayString = "Invalid day";
+                break;
+        }
+
+        System.out.println(dayString);
+    }
+}
