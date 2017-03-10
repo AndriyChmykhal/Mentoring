d25c5165fe73d4aa9b13f248ace8e968e4f5291b
Andriy_Chmykhal <chmykhal@ukr.net>
2017-03-10 15:43:43 +0200

    BibarySearchArr and BubbleSorting

--- src/com/learning/BubbleSorting.java
+++ src/com/learning/BubbleSorting.java
@@ -1,17 +1,25 @@
+
+
 package com.learning;
+import java.util.Arrays;
 
  public class BubbleSorting {
     public static void main(String[] args) {
-        int arr[] = {10, 1, 3, 5, 4, 7, 6, 9, 8, 2};
-             for (int i = 0; i < arr.length; i--) {
-                for (int j = 1; j < arr.length-i; j++) {
+        int arr[] = {10, 1, 8, 2, -1, 14, 10};
+             for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length-1; j++) {
                     if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                    }
+
             }
-       }System.out.print(arr);
+
+       }
+        System.out.println(Arrays.toString(arr));
+
 
 
 
