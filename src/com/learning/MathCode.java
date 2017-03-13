812aada3d5411e6b00d32da656319b1c131a61c4
Andriy_Chmykhal <chmykhal@ukr.net>
2017-03-13 15:48:12 +0200

    Deleting separate method and refactoring

--- src/com/learning/MathCode.java
+++ src/com/learning/MathCode.java
@@ -1,36 +1,29 @@
 package com.learning;
-
 public class MathCode {
-
     public static void main(String[] args) {
-
-        calc(1.00, 0.00, 0.00);
-    }
-
-    public static void calc(double a, double b, double c) {
-
+        double a = 0;
+        double b = 2;
+        double c = 3;
         if (a == 0) {
             System.out.println("Parameter 'a' cannot be equal to zero for such formula. Please, change it for another value");
-        }
-
-        double x1, x2, x;
-        double D = (b * b - 4 * a * c);
-
-        x = -(b / (2 * a));
-        x1 = (-b - Math.sqrt(D)) / (2 * a);
-        x2 = (-b + Math.sqrt(D)) / (2 * a);
-
-        if (D < 0) {
-            System.out.println("For such parameters discriminant value is negative. In this case there are no roots");
-        } else if (D == 0) {
-            if (b != 0) {
-                System.out.println("For such parameters discriminant value equals to zero. In this case there is only one root x = " + x);
-            } else {
-                System.out.println("For such parameters discriminant value equals to zero. In this case x has only one root x = " + (-x));
-            }
         } else {
-            System.out.println("The first root of formula x1 = " + x1);
-            System.out.println("The second root of formula x2 = " + x2);
+            double x1, x2, x;
+            double D = (b * b - 4 * a * c);
+            x = -(b / (2 * a));
+            x1 = (-b - Math.sqrt(D)) / (2 * a);
+            x2 = (-b + Math.sqrt(D)) / (2 * a);
+            if (D < 0) {
+                System.out.println("For such parameters discriminant value is negative. In this case there are no roots");
+            } else if (D == 0) {
+                if (b != 0) {
+                    System.out.println("For such parameters discriminant value equals to zero. In this case there is only one root x = " + x);
+                } else {
+                    System.out.println("For such parameters discriminant value equals to zero. In this case x has only one root x = " + (-x));
+                }
+            } else {
+                System.out.println("The first root of formula x1 = " + x1);
+                System.out.println("The second root of formula x2 = " + x2);
+            }
         }
     }
 }
