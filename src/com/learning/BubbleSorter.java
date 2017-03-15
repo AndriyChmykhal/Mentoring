09f4ff968073728741417daa0fae776e9325603e
Andriy_Chmykhal <chmykhal@ukr.net>
2017-03-15 17:13:12 +0200

    bubbleSort update

--- src/com/learning/BubbleSorter.java
+++ src/com/learning/BubbleSorter.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class BubbleSorter {
     public static int[] sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length - 1 -i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
