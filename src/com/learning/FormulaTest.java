3e217692a2308e9e5d8516a31ad006e185dc5f9c
Andriy <chmykhal@ukr.net>
2017-05-10 01:30:46 +0300

    task7

---
+++ src/com/learning/FormulaTest.java
@@ -0,0 +1,21 @@
+package com.learning;
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class FormulaTest {
+
+
+    @Test
+    public void testCalculate() {
+        Formula formula = new Formula;
+        formula.calculate();   }
+}
+
+
+
+
+
+
+
---
+++ src/com/learning/Launcher.java
@@ -0,0 +1,7 @@
+package com.learning;
+public class Launcher {
+    public static void main(String[] args) {
+    Formula formula = new Formula();
+        formula.calculate(0, 2, 3);
+    }
+}
