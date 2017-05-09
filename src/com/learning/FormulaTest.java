87bac071f4094d761a3aac1d45d7122f0df1baf9
Andriy <chmykhal@ukr.net>
2017-05-10 01:47:56 +0300

    task7

--- src/com/learning/FormulaTest.java
+++ src/com/learning/FormulaTest.java
@@ -1,7 +1,10 @@
 package com.learning;
 
 
+
 import static org.junit.Assert.assertEquals;
+
+import junit.framework.Assert;
 import org.junit.Test;
 
 public class FormulaTest {
@@ -9,9 +12,14 @@ public class FormulaTest {
 
     @Test
     public void testCalculate() {
-        Formula formula = new Formula;
-        formula.calculate();   }
+        Formula formula = new Formula();
+        Assert.assertEquals("Parameter 'a' cannot be equal to zero for such formula. Please, change it for another value", formula.calculate(0, 2, 3));
+
+
+    }
+
 }
+
 
 
 
