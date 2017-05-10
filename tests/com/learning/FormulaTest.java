05e7af1dc5316fdb72709249183e58c6911fa5d6
Andriy_Chmykhal <chmykhal@ukr.net>
2017-05-10 11:36:36 +0300

    task7

---
+++ tests/com/learning/FormulaTest.java
@@ -0,0 +1,18 @@
+package com.learning;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Andriy_Chmykhal on 5/10/2017.
+ */
+public class FormulaTest {
+    @Test
+    public void calculate() throws Exception {
+
+    }
+
+}
+
+
