e960fa211d461ee922413db0328fa56c7e35c472
Andriy_Chmykhal <chmykhal@ukr.net>
2017-05-25 12:33:43 +0300

    Maven project and tests

--- src/test/java/com/learning/FormulaExeptionsTest.java
+++ src/test/java/com/learning/FormulaExeptionsTest.java
@@ -3,12 +3,12 @@ package com.learning;
 import org.junit.Test;
 
 public class FormulaExeptionsTest {
-    @Test(expected = Exception.class, timeout = 100)
+    @Test(expected = NullPointerException.class, timeout = 100)
     public void testAEqualsZero() {
         Formula formula = new Formula();
         formula.calculatex1(0, 2, 1);
     }
-    @Test(expected = Exception.class, timeout = 100)
+    @Test(expected = NullPointerException.class, timeout = 100)
     public void testDLessZero() {
         Formula formula = new Formula();
         formula.calculatex1(0.5, 1, 1);
